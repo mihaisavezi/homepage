9cd0d47d7a9f517aa23a2c9ade21b9a9eb220247
Mihai Popescu <mihai.popescu@dhcp069.buh1.avira.org>
2018-04-04 13:18:19 +0300

    split header into separate component

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,36 +2,17 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
-import Header from '../components/Header'
+import Header from '../components/Header/index.js';
 import './index.less'
-import logo from '../assets/logo.png';
 
 const TemplateWrapper = ({ children }) => (
   <div className="bg-blue-1">
         <div className="mw9 center min-vh-100 flex flex-column">
-          <nav className="row pb3 w-100 flex-m flex-l pa4-m pa5-l flex-row items-center justify-between">
-            <div className="left">
-              <img src={logo} className="h3 w3 center mb3 mb0-ns db mt2 mt0-ns" alt="" srcSet=""/>
-            </div>
-            <div className="right flex items-center justify-center justify-end-ns">
-              <div className="flex ph4-ns">
-                <p className="f6 athelas ttc ph2 ph3-ns blue-7">home</p>
-                <p className="f6 athelas ttc ph2 ph3-ns blue-7">projects</p>
-              </div>
-              <div className="h2 w2 mh2">
-                <a href="https://github.com/mihaisavezi" className="blue-7 h-100 flex flex-column justify-center items-center">
-                  <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={1} strokeLinecap="round" strokeLinejoin="round" className="feather feather-github"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22" /></svg>
-                </a>
-              </div>
-              <a href="https://www.linkedin.com/" className="blue-7 h-100 flex flex-column justify-center items-center">
-                  <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={1} strokeLinecap="round" strokeLinejoin="round" className="feather feather-linkedin"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z" /><rect x={2} y={9} width={4} height={12} /><circle cx={4} cy={4} r={2} /></svg>
-                </a>
-            </div>
-          </nav>
+          <Header/>
           <div className="flex flex-column flex-auto justify-center-l">
             <div className="h-75 center w-100 pa5-l">
-              <div className="h-100 w-100 bg-white-80 flex flex-column flex-row-m flex-row-l pa4 ba-l b--blue-2">
-                <div className="w-100 w-third-m w-third-l pa3 pa5-l flex flex-column">
+              <div className="h-100 w-100 bg-white-80 flex flex-column flex-row-m flex-row-l pa3 pa4-ns ba-l b--blue-2">
+                <div className="w-100 w-third-m w-third-l pa0 pa3-ns pa5-l flex flex-column">
                   <div className="h4 w4 h5-l w5-l self-center">
                     <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 64 64" enableBackground="new 0 0 64 64" xmlSpace="preserve">
                       <g>
@@ -70,11 +51,11 @@ const TemplateWrapper = ({ children }) => (
                     </svg>
                   </div>
                 </div>
-                <div className="w-100 w-two-thirds-m w-two-thirds-l pa3 pa5-l flex flex-column justify-center items-start">
+                <div className="w-100 w-two-thirds-m w-two-thirds-l pa2 pa3-ns pa5-l flex flex-column justify-center items-start">
                   <h2 className="f1 f-headline-l lh-solid blue-6">I build the stuff you see</h2>
-                  <p className="lh-title f6 f5-ns pt4 gray-5 dn db-ns">with tried and tested web technologies:<span className="ttu ph2 bt-0 bl-0 br-0 bb b--dotted tracked avenir f7">html5</span>
-                    <span className="ttu ph2 bt-0 bl-0 br-0 bb b--dotted tracked avenir f7">css3</span>
-                    <span className="ttu ph2 bt-0 bl-0 br-0 bb b--dotted tracked avenir f7">javascript</span>
+                  <p className="lh-title f6 f5-ns pt4 gray-5 dn db-ns">with tried and tested web technologies:<span className="dib ttu ph2 bt-0 bl-0 br-0 bb b--dotted tracked avenir f7 mr2">html5</span>
+                    <span className="dib ttu ph2 bt-0 bl-0 br-0 bb b--dotted tracked avenir f7 mr2">css3</span>
+                    <span className="dib ttu ph2 bt-0 bl-0 br-0 bb b--dotted tracked avenir f7 mr2">javascript</span>
                   </p>
                   <div className="db dn-ns w-100 gray-5">
                     <p className="lh-title f5 f5-ns pb4 pt2 gray-5">with tried and tested web technologies:</p>
@@ -88,7 +69,10 @@ const TemplateWrapper = ({ children }) => (
                       <span className="ttu bt-0 bl-0 br-0 bb b--dotted tracked avenir f7">javascript</span>
                     </div>
                   </div>
-                  <a className="bg-blue-6 near-white pa3 ph5 tc dib mt5 helvetica fw4 ba b--blue-2 f5 ml-auto mr-auto ml0-ns">Hire me</a>
+                  <a
+                  href="mailto:contactmihaip@gmail.com?Subject=Let's%20chat"
+                  target="_top"
+                  className="no-underline bg-blue-6 near-white pa3 ph5-ns tc dib mt5 helvetica fw4 ba b--blue-2 f5 ml-auto mr-auto ml0-ns">Hire me</a>
                 </div>
               </div>
             </div>

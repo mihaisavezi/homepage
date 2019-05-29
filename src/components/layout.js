5a937d3076663302996ef9655c1e596adbf7ade7
MIHAI POPESCU <mihai.ppscu@gmail.com>
2019-05-29 22:55:05 +0300

    init: first commit of rewrite with gatsby 2

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,7 +10,7 @@ import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
-import "./layout.css"
+import "../assets/styles/index.less"
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -25,21 +25,11 @@ const Layout = ({ children }) => (
     `}
     render={data => (
       <>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
+        <div className="bg-blue-1">
+          <div className="mw9 center min-vh-100 flex flex-column">
+            <Header />
+            {children}
+          </div>
         </div>
       </>
     )}

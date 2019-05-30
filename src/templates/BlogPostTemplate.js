6d774200c41a31073fb4fd2d6939d47cba3a12f3
MIHAI POPESCU <mihai.ppscu@gmail.com>
2019-05-30 22:53:47 +0300

    fix: styling

--- src/templates/BlogPostTemplate.js
+++ src/templates/BlogPostTemplate.js
@@ -1,4 +1,5 @@
 import React from "react"
+import BlogLayout from "../components/blogLayout"
 
 export default function Template({
   data, // this prop will be injected by the GraphQL query below.
@@ -6,16 +7,18 @@ export default function Template({
   const { markdownRemark } = data // data.markdownRemark holds our post data
   const { frontmatter, html } = markdownRemark
   return (
-    <div className="blog-post-container">
-      <div className="blog-post">
-        <h1>{frontmatter.title}</h1>
-        <h2>{frontmatter.date}</h2>
-        <div
-          className="blog-post-content"
-          dangerouslySetInnerHTML={{ __html: html }}
-        />
+    <BlogLayout>
+      <div className="blog-post-container">
+        <div className="blog-post">
+          <h1>{frontmatter.title}</h1>
+          <h2>{frontmatter.date}</h2>
+          <div
+            className="blog-post-content"
+            dangerouslySetInnerHTML={{ __html: html }}
+          />
+        </div>
       </div>
-    </div>
+    </BlogLayout>
   )
 }
 

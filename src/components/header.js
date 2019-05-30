5d3a8a42a29fb862e6d1f5e93e2551f752b5b204
MIHAI POPESCU <mihai.ppscu@gmail.com>
2019-05-30 22:58:03 +0300

    fix: header and postlink

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,7 @@
-import React from "react"
-import Link from "gatsby-link"
-import logo from "../images/logo.png"
+import React from 'react'
+import Link from 'gatsby-link'
+import logo from '../assets/logo-2.png';
+
 
 const Header = () => (
   <nav className="row pb3 w-100 flex-m flex-l pa4-m pa5-l flex-row items-center justify-between">
@@ -15,64 +16,19 @@ const Header = () => (
     </div>
     <div className="right flex items-center justify-center justify-end-ns">
       <div className="flex ph4-ns">
-        <p className="text-link text-link__isActive f6 athelas ttc pa2 pa3-ns pb2-ns gray-4 mr1">
-          home
-        </p>
-        <p className="text-link f6 athelas ttc pa2 pa3-ns pb2-ns gray-4">
-          projects
-        </p>
-        <Link
-          className="text-link f6 athelas ttc pa2 pa3-ns pb2-ns gray-4 no-underline"
-          to="/blog"
-        >
-          blog
-        </Link>
-        <p className="text-link f6 athelas ttc pa2 pa3-ns pb2-ns gray-4">
-          reading list
-        </p>
+        <p className="text-link text-link__isActive f6 athelas ttc pa2 pa3-ns pb2-ns gray-4 mr1">home</p>
+        <p className="text-link f6 athelas ttc pa2 pa3-ns pb2-ns gray-4">projects</p>
+        <Link className="text-link f6 athelas ttc pa2 pa3-ns pb2-ns gray-4 no-underline" to='/blog'>blog</Link>
+        <p className="text-link f6 athelas ttc pa2 pa3-ns pb2-ns gray-4">reading list</p>
       </div>
       <div className="h2 w2 mh2">
-        <a
-          href="https://github.com/mihaisavezi"
-          className="gray-4 h-100 flex flex-column justify-center items-center"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width={24}
-            height={24}
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth={1}
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className="feather feather-github"
-          >
-            <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22" />
-          </svg>
+        <a href="https://github.com/mihaisavezi" className="gray-4 h-100 flex flex-column justify-center items-center">
+          <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={1} strokeLinecap="round" strokeLinejoin="round" className="feather feather-github"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22" /></svg>
         </a>
       </div>
-      <a
-        href="www.linkedin.com/in/contactmihaip"
-        className="gray-4 h-100 flex flex-column justify-center items-center"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width={24}
-          height={24}
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth={1}
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          className="feather feather-linkedin"
-        >
-          <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z" />
-          <rect x={2} y={9} width={4} height={12} />
-          <circle cx={4} cy={4} r={2} />
-        </svg>
-      </a>
+      <a href="www.linkedin.com/in/contactmihaip" className="gray-4 h-100 flex flex-column justify-center items-center">
+          <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={1} strokeLinecap="round" strokeLinejoin="round" className="feather feather-linkedin"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z" /><rect x={2} y={9} width={4} height={12} /><circle cx={4} cy={4} r={2} /></svg>
+        </a>
     </div>
   </nav>
 )
--- src/components/postLink.js
+++ src/components/postLink.js
@@ -1,16 +1,20 @@
-import React from "react"
-import Link from "gatsby-link"
+import React from "react";
+import Link from "gatsby-link";
 
 const PostLink = ({ post }) => (
   <div>
     <Link className="no-underline" to={post.frontmatter.path}>
       <div>
-        <h3 className="f3 di blue-6">{post.frontmatter.title} —</h3>
-        <h3 className="di gray-3"> {post.frontmatter.date}</h3>
+        <h3 className="f3 di blue-6">
+          {post.frontmatter.title} —
+        </h3>
+        <h3 className="di gray-3">
+          {" "}{post.frontmatter.date}
+        </h3>
       </div>
       <p className="blue-6 mt2 tl">{post.excerpt}</p>
     </Link>
   </div>
-)
+);
 
-export default PostLink
+export default PostLink;

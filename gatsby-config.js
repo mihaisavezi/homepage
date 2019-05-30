9814ff07b747c7812d6838f7b666d923b7346a13
MIHAI POPESCU <mihai.ppscu@gmail.com>
2019-05-30 10:06:38 +0300

    feat: add typography plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,12 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-less`,
     {
+      resolve: "gatsby-plugin-typography",
+      options: {
+        pathToConfigModule: "src/utils/typography.js",
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
--- src/components/layout.js
+++ src/components/layout.js
@@ -5,35 +5,22 @@
  * See: https://www.gatsbyjs.org/docs/static-query/
  */
 
+import "../assets/styles/index.less"
 import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
-import "../assets/styles/index.less"
 
 const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `}
-    render={data => (
-      <>
-        <div className="bg-blue-1">
-          <div className="mw9 center min-vh-100 flex flex-column">
-            <Header />
-            {children}
-          </div>
-        </div>
-      </>
-    )}
-  />
+  <>
+    <div className="bg-blue-1">
+      <div className="mw9 center min-vh-100 flex flex-column">
+        <Header />
+        {children}
+      </div>
+    </div>
+  </>
 )
 
 Layout.propTypes = {
